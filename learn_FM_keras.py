df2b9d9d80e8bb8db37baff16d6346f1787abd03
jennigato <jennifer.jiang18@imperial.ac.uk>
2019-02-28 15:52:51 +0000

    code working with keras model and can predict hidden

--- learn_FM_keras.py
+++ learn_FM_keras.py
@@ -1,101 +1,124 @@
 from __future__ import print_function
 import numpy as np
 import csv
+import pickle
 
 import keras
-from keras.datasets import mnist
 from keras.models import Sequential
-from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Dense, Activation, Dropout
+from keras.layers.normalization import BatchNormalization
 from keras.wrappers.scikit_learn import KerasRegressor
 from keras.optimizers import SGD
 from keras.constraints import maxnorm
 
-from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import RandomizedSearchCV
 
 from nn_lib import Preprocessor
-#    MultiLayerNetwork,
-#    Trainer,
-#    save_network,
-#    load_network,
-#)
+
 from illustrate import illustrate_results_FM
 from sklearn.metrics import mean_squared_error
 
 
 def main(_neurons, _activationFunctionHidden, _activationFunctionOutput, _lossFunction, _batchSize, _learningRate,
-         _numberOfEpochs, _writeToCSV=False):
+         _numberOfEpochs, _writeToCSV=False, _Train=True):
 
     dataset = np.loadtxt("FM_dataset.dat")
 
     #######################################################################
     #                       ** START OF YOUR CODE **
     #######################################################################
-    # Setup hyperparameters and neural network
-    input_dim = 3  # CONSTANT: Stated in specification
-    neurons = _neurons
-    activations = _activationFunctionHidden
-    #net = MultiLayerNetwork(input_dim, neurons, activations)
-    np.random.shuffle(dataset)
 
-    # Separate data columns into x (input features) and y (output)
-    x = dataset[:, :input_dim]
-    y = dataset[:, input_dim:]
+    if _Train == True:
 
-    split_idx = int(0.8 * len(x))
+        input_dim = 3 # CONSTANT: Stated in specification
 
-    # Split data by rows into a training set and a validation set
-    x_train = x[:split_idx]
-    y_train = y[:split_idx]
-    x_val = x[split_idx:]
-    y_val = y[split_idx:]
+        #shuffle the data
+        np.random.shuffle(dataset)
 
-    # Apply preprocessing to the data
-    prep_input = Preprocessor(x_train)
-    x_train_pre = prep_input.apply(x_train)
-    x_val_pre = prep_input.apply(x_val)
+        # Separate data columns into x (input features) and y (output)
+        x = dataset[:, :input_dim]
+        y = dataset[:, input_dim:]
 
-    # fix random seed for reproducibility
-    seed = 7
-    np.random.seed(seed)
+        split_idx = int(0.8 * len(x))
 
-    #create model
-    model = KerasRegressor(build_fn=create_model, nb_epoch=_numberOfEpochs,
-                            batch_size=_batchSize)
+        # Split data by rows into a training set and a validation set
+        x_train = x[:split_idx]
+        y_train = y[:split_idx]
+        x_val = x[split_idx:]
+        y_val = y[split_idx:]
 
-    # Use scikit-learn to grid search - these are all possible paramaters, takes a long time so I only left in few values
-    batch_size = [8, 16] #32
-    activation =  ['relu', 'tanh', 'sigmoid']
-    epochs = [1, 10] #100, 250, 500, 1000?
-    learn_rate = [1e-1, 1e-3, 1e-6]
-    dropout_rate = [0.0, 0.5, 0.9]
-    neurons = [1, 5, 10, 15]
-    optimizer = [ 'SGD', 'RMSprop', 'Adam']
+        # Apply preprocessing to the data
+        x_prep_input = Preprocessor(x_train)
+        y_prep_input = Preprocessor(y_train)
 
-    param_grid = dict(epochs=epochs, batch_size=batch_size, learn_rate=learn_rate,
-                        activation=activation, neurons=neurons)
+        x_train_pre = x_prep_input.apply(x_train)
+        y_train_pre = y_prep_input.apply(y_train)
 
-    grid = GridSearchCV(estimator=model, param_grid=param_grid, n_jobs=-1, cv=10)
-
-    grid_result = grid.fit(x_train_pre, y_train)
-
-    # summarize results
-
-    print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
-    means = grid_result.cv_results_['mean_test_score']
-    stds = grid_result.cv_results_['std_test_score']
-    params = grid_result.cv_results_['params']
-    for mean, stdev, param in zip(means, stds, params):
-        print ("%f (%f) with: %r" % (mean, stdev, param))
+        x_val_pre = x_prep_input.apply(x_val)
+        y_val_pre = y_prep_input.apply(y_val)
 
 
-    ##########this is not working yet, find out how to predict with keras models
-    # Evaluate the neural network
-    #preds = grid.predict(x_val_pre)
-    #targets = y_val
-    #mse = evaluate_architecture(targets, preds)
-    print("Mean squared error:", mse)
+        # fix random seed for reproducibility
+        seed = 7
+        np.random.seed(seed)
 
-    predict_hidden(dataset)
+        #create model
+        model = KerasRegressor(build_fn=create_model,
+                                nb_epoch=_numberOfEpochs,
+                                batch_size=_batchSize)
+
+        # Use scikit-learn to grid search - these are all possible paramaters, takes a long time so I only left in few values
+        batch_size = [8, 16, 32] #32
+        epochs = [1] #10, 100, 250, 500, 1000?
+        learn_rate = [1e-1, 1e-3, 1e-6]
+        neurons = [1, 5, 15]
+        hidden_layers = [3, 4, 5]
+        #activation =  ['relu', 'sigmoid'] #tanh
+
+        #optimizer = [ 'SGD', 'RMSprop', 'Adam']
+        #dropout_rate = [0.0, 0.5, 0.9]
+
+        param_grid = dict(epochs=epochs,
+                            batch_size=batch_size,
+                            learn_rate=learn_rate,
+                            neurons=neurons,
+                            hidden_layers=hidden_layers)
+
+        #perform grid search with 10-fold cross validation
+        grid = RandomizedSearchCV(estimator=model,
+                            param_distributions=param_grid,
+                            n_jobs=-1,
+                            cv=10)
+
+        grid_result = grid.fit(x_train_pre, y_train_pre)
+
+
+        #summarize results of hyperparameter search
+        print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
+        means = grid_result.cv_results_['mean_test_score']
+        stds = grid_result.cv_results_['std_test_score']
+        params = grid_result.cv_results_['params']
+        for mean, stdev, param in zip(means, stds, params):
+            print ("%f (%f) with: %r" % (mean, stdev, param))
+
+
+        #extract the best model
+        best_model = grid.best_estimator_.model
+
+        #Evaluate the best model
+        preds = best_model.predict(x_val_pre)
+        targets = y_val_pre
+        mse = evaluate_architecture(targets, preds)
+        print("Mean squared error of best model:", mse)
+
+        #save the best model
+        filename = 'trained_FM.pickle'
+        pickle.dump(best_model, open(filename, 'wb'))
+
+
+    #predict hidden dataset using best model
+    predictions = predict_hidden(dataset)
+    print(predictions)
 
     #######################################################################
     #                       ** END OF YOUR CODE **
@@ -104,38 +127,48 @@ def main(_neurons, _activationFunctionHidden, _activationFunctionOutput, _lossFu
 
 #create model for KerasRegressor - right now the network is just input output layer, figure out
 #how to modify number of hidden layers maybe
-def create_model(neurons=1, learn_rate=0.01, activation='relu'):
+def create_model(neurons=1, learn_rate=0.01, activation='relu', hidden_layers=1):
     # default values
-    dropout_rate=0.0 # or 0.2
-    optimizer='adam' # or SGD
-    input_dim = 3
-
+    input_dim = 3  # CONSTANT: Stated in specification
     # create model
     model = Sequential()
-    model.add(Dense(3, input_dim=input_dim, activation=activation))
+    #add input layer with batch normalization
+    model.add(Dense(3, input_dim=input_dim))
+    model.add(BatchNormalization())
+    model.add(Activation(activation))
+    #add hidden layers
+    for i in range(hidden_layers):
+        model.add(Dense(4))
+        model.add(BatchNormalization())
+        model.add(Activation(activation))
+
+    #add output layer
     model.add(Dense(3))
-    # Compile model
+    model.add(BatchNormalization())
+
+    #compile model
     model.compile(loss='mean_squared_error', optimizer='adam')
 
     return model
 
-#how to load best parameter in here?
+
 def predict_hidden(dataset):
 
-    filePath = "trained_ROI.pickle"
     input_dim = 3
 
     # Pre-process data
-    xData = inputData[:, :input_dim]
-    yData = inputData[:, input_dim:]
-    prep_input = Preprocessor(xData)
-    x_data_pre = prep_input.apply(xData)
+    x_data = dataset[:, :input_dim]
+    y_data = dataset[:, input_dim:]
+    prep_input = Preprocessor(x_data)
+    x_data_pre = prep_input.apply(x_data)
 
     # Load the network
-    #net = load_network(filePath)
+    filename = open("trained_FM.pickle", 'rb')
+    model = pickle.load(filename)
+    filename.close()
 
     # Generate the output
-    #preds = net(x_data_pre) #use keras to make prediction
+    preds = model.predict(x_data_pre) #use keras to make prediction
 
 
     return preds
